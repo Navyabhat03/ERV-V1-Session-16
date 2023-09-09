36c878c635f9f512071ff6c0cabde0b5f2beed9f
Navyabhat03 <60884505+Navyabhat03@users.noreply.github.com>
2023-09-09 10:04:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 10,
+        "batch_size": 64,
         "num_epochs": 40,
         "lr": 10**-3,
         "d_model": 512,
